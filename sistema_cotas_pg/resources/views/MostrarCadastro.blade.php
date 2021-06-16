e5c96686232fbaebf0543ace49d9f5332eb4b7b8
Abdiel <abdieldeathayde67@gmail.com>
2021-06-16 13:21:16 -0300

    feat: add new file env

---
+++ sistema_cotas_pg/resources/views/MostrarCadastro.blade.php
@@ -0,0 +1,7 @@
+<h3>Informações do aluno</h3>
+<p>ID: {{ $aluno['id'] }} </p>
+<p>Nome: {{$aluno['nome'] }} </p>
+<p>Idade: {{$aluno['idade'] }} </p>
+<p>Curso: {{$aluno['curso'] }} </p>
+<p>Email: {{$aluno['email'] }} </p>
+<br>
